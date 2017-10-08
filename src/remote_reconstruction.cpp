47f9e6053484e1964ba217044aace77971927977
rdiederichse <rdiederichse@uos.de>
2017-10-08 14:30:55 +0200

    Add reconstruction server for calls to cluster instead of doing it locally

---
+++ src/remote_reconstruction.cpp
@@ -0,0 +1,12 @@
+#include "lvr_ros/reconstruction.h"
+
+namespace lvr_ros
+{
+
+    class RemoteReconstruction : public Reconstruction
+    {
+        public:
+            RemoteReconstruction()  : Reconstruction() {}
+
+    };
+}
