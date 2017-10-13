2cbba674736b673dbe2efb9ee44d2e982806dca0
rdiederichse <rdiederichse@uos.de>
2017-10-13 16:52:54 +0200

    Refactoring; Send Trigger after 2 clouds

--- src/remote_reconstruction_client.cpp
+++ src/remote_reconstruction_client.cpp
@@ -8,48 +8,82 @@
 #include "lvr_ros/SendCloudAction.h"
 #include "lvr_ros/StartReconstructionAction.h"
 
-ros::Subscriber cloud_sub;
-boost::shared_ptr<actionlib::SimpleActionClient<lvr_ros::SendCloudAction> > client_send_ptr;
-boost::shared_ptr<actionlib::SimpleActionClient<lvr_ros::StartReconstructionAction> > client_reconstruct_ptr;
+using namespace lvr_ros;
 
-void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud)
+class CloudClient
 {
-    ROS_INFO_STREAM("Received cloud.");
-    lvr_ros::SendCloudGoal goal;
-    goal.cloud = *cloud;
-    client_send_ptr->sendGoal(goal);
+    private:
+        ros::Subscriber cloud_sub;
+        /// action clients
+        actionlib::SimpleActionClient<SendCloudAction> client_send;
+        actionlib::SimpleActionClient<StartReconstructionAction> client_reconstruct;
+        int n_clouds;
 
-    geometry_msgs::PoseStamped robot_pose;
-    tf::TransformListener tf_listener;
-    const std::string robot_frame = "riegl_meas_origin";
-    const std::string global_frame = "odom_combined";
-    move_base_flex::getRobotPose(tf_listener, robot_frame, global_frame, ros::Duration(2), robot_pose);
-    goal.pose = robot_pose;
+        void sendTrigger()
+        {
+            StartReconstructionGoal goal;
+            client_reconstruct.sendGoal(goal);
+            bool result = client_reconstruct.waitForResult(ros::Duration(5));
+            if (not result)
+            {
+                ROS_ERROR_STREAM("Trigger not sent.");
+                n_clouds = 0;
+            } else
+            {
+                ROS_INFO_STREAM("Trigger sent successfully.");
+            }
+        }
 
-    ROS_INFO_STREAM("Sending cloud...");
-    bool result = client_send_ptr->waitForResult(ros::Duration(5));
-    if (not result)
+        void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud)
+        {
+            ROS_INFO_STREAM("Received cloud.");
+            SendCloudGoal goal;
+            goal.cloud = *cloud;
+            client_send.sendGoal(goal);
+
+            geometry_msgs::PoseStamped robot_pose;
+            tf::TransformListener tf_listener;
+            const std::string robot_frame = "riegl_meas_origin";
+            const std::string global_frame = "odom_combined";
+            move_base_flex::getRobotPose(tf_listener, robot_frame, global_frame, ros::Duration(2), robot_pose);
+            goal.pose = robot_pose;
+
+            ROS_INFO_STREAM("Sending cloud...");
+            bool result = client_send.waitForResult(ros::Duration(5));
+            if (not result)
+            {
+                ROS_ERROR_STREAM("Cloud not sent.");
+            } else
+            {
+                ++n_clouds;
+                ROS_INFO_STREAM("Cloud sent successfully.");
+            }
+
+            if (n_clouds >= 2)
+            {
+                sendTrigger();
+            }
+
+        }
+
+    public:
+        CloudClient(ros::NodeHandle& nh) : client_send("/send"), client_reconstruct("/reconstruct"), n_clouds(0)
     {
-        ROS_ERROR_STREAM("Cloud not sent.");
-    } else
-    {
-        ROS_INFO_STREAM("Cloud sent successfully.");
+        cloud_sub = nh.subscribe<sensor_msgs::PointCloud2>("/riegl_cloud", 1,
+                &CloudClient::pointCloudCallback, this);
+        ROS_INFO_STREAM("Waiting for servers.");
+        client_send.waitForServer();
+        client_reconstruct.waitForServer();
     }
+};
 
-}
 
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "remote_reconstruction_client");
     ros::NodeHandle nh("~");
-    client_send_ptr = boost::shared_ptr<actionlib::SimpleActionClient<lvr_ros::SendCloudAction> >
-        (new actionlib::SimpleActionClient<lvr_ros::SendCloudAction>("/send"));
-    client_reconstruct_ptr = boost::shared_ptr<actionlib::SimpleActionClient<lvr_ros::StartReconstructionAction> >
-        (new actionlib::SimpleActionClient<lvr_ros::StartReconstructionAction>("/reconstruct"));
+    static CloudClient client(nh);
     ROS_INFO_STREAM("Started cloud client.");
-    cloud_sub = nh.subscribe<sensor_msgs::PointCloud2>("/riegl_cloud", 1, pointCloudCallback);
-    client_send_ptr->waitForServer();
-    client_reconstruct_ptr->waitForServer();
     ros::spin();
     return 0;
 }
