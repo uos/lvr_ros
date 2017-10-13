0ca21b640bd345460fcc98dae425261eb516f978
rdiederichse <rdiederichse@uos.de>
2017-10-13 13:46:17 +0200

    Query robot pose

--- src/remote_reconstruction_client.cpp
+++ src/remote_reconstruction_client.cpp
@@ -1,6 +1,9 @@
 #include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
+#include <tf/transform_listener.h>
 #include <sensor_msgs/PointCloud2.h>
+
+#include "move_base_flex/navigation_utility.h"
 
 #include "lvr_ros/SendCloudAction.h"
 #include "lvr_ros/StartReconstructionAction.h"
@@ -15,8 +18,15 @@ void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud)
     lvr_ros::SendCloudGoal goal;
     goal.cloud = *cloud;
     client_send_ptr->sendGoal(goal);
-    ROS_INFO_STREAM("Sent cloud");
 
+    geometry_msgs::PoseStamped robot_pose;
+    tf::TransformListener tf_listener;
+    const std::string robot_frame = "riegl_meas_origin";
+    const std::string global_frame = "odom_combined";
+    move_base_flex::getRobotPose(tf_listener, robot_frame, global_frame, ros::Duration(2), robot_pose);
+    goal.pose = robot_pose;
+
+    ROS_INFO_STREAM("Sending cloud...");
     bool result = client_send_ptr->waitForResult(ros::Duration(5));
     if (not result)
     {
