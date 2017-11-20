5b5562a67c59e694ad6e4095dc306242cf0a8445
Rasmus Diederichsen <rdiederichse@uos.de>
2017-11-20 12:15:35 +0100

    Complete FileObserver class

--- include/lvr_ros/FileObserver.hpp
+++ include/lvr_ros/FileObserver.hpp
@@ -3,10 +3,16 @@
 
 #include <sys/inotify.h>
 #include <boost/filesystem.hpp>
+#include <functional>
 #include <thread>
+#include <mutex>
 
 namespace bfs = boost::filesystem;
 
+/**
+ * @enum FSEventType
+ * @brief Represents the kind of event happening to a file.
+ */
 typedef enum
 {
     CREATED = 0,
@@ -14,23 +20,41 @@ typedef enum
     DELETED = 2
 } FSEventType;
 
+/**
+ * @struct FSEvent
+ * @brief POD for transporting information about an event.
+ */
 struct FSEvent
 {
-    const bfs::path file;
-    const FSEventType type;
+    bfs::path file; ///< path to the affected file
+    FSEventType type; ///< wvent kind
 };
 
 // Function type for callback
-typedef void (*EventCallback)(const FSEvent&);
+typedef std::function<void(const FSEvent&)> EventCallback;
 
+/**
+ * @class FileObserver
+ * @brief A Class that observes a filesystem path and registers callbacks which
+ * are invoked for the specified event (create, modify, delete)
+ * @note This is LLinux only
+ */
 class FileObserver
 {
     private:
-        bfs::path path;
-        EventCallback created_cb;
-        EventCallback modfied_cb;
-        EventCallback deleted_cb;
-        std::thread runner;
+        bfs::path path;             ///< path to observe
+        EventCallback created_cb;   ///< callback for file creation
+        EventCallback modified_cb;  ///< callback for file contents modification (not `touch`)
+        EventCallback deleted_cb;   ///< callback for file deletion
+        std::thread runner;         ///< background thread polling inotify for file events
+        std::mutex mutex;           ///< mutex for supervision read/write to callback members
+        bool error;                 ///< error flag set by the thread when it can't do its work
+
+        /**
+         * @brief Oberve the path for file system events. Executed by the runner
+         * thread
+         */
+        void observe();
 
     public:
 		virtual ~FileObserver()
@@ -38,57 +62,43 @@ class FileObserver
             // TODO: Stop runner thread?
         }
 
-        FileObserver(const bfs::path& path) : path(path), created_cb(nullptr),
-                                               modfied_cb(nullptr), deleted_cb(nullptr)
+        /**
+         * @brief Create a new FileObserver. This will null-initialise all
+         * callbacks. The runner thread starts immediately.
+         */
+        FileObserver(const bfs::path& path) :
+            path(path),
+            created_cb(nullptr),
+            modified_cb(nullptr),
+            deleted_cb(nullptr),
+            runner(std::bind(&FileObserver::observe, this)),
+            error(false)
         {
-            this->runner = std::thread(boost::bind(&FileObserver::observe, this));
             runner.detach();
         }
 
-        void observe()
-        {
-            int fd = inotify_init();
-            int descriptor = inotify_add_watch(fd, this->path.string().c_str(), IN_MODIFY | IN_CREATE | IN_DELETE);
-            if (descriptor < 0)
-            {
-                // TODO: Error
-            } else
-            {
-                while (true)
-                {
-                    char buf[1024];
-                    int len;
-                    len = read(fd, buf, 1024);
-					struct inotify_event* event = (struct inotify_event *) buf;
-                    if (event->mask & IN_MODIFY)
-                    {
-                        std::cout << std::string(event->name) << " was modified." << std::endl;
-                    }
-                    if (event->mask & IN_CREATE)
-                    {
-                        std::cout << std::string(event->name) << " was created." << std::endl;
-                    }
-                    if (event->mask & IN_DELETE)
-                    {
-                        std::cout << std::string(event->name) << " was deleted." << std::endl;
-                    }
-				}
-            }
-        }
-        void on_created(EventCallback& callback)
-        {
-            this->created_cb = callback;
-        }
+        /**
+         * @brief Register function to be called when a file is created.
+         * @param callback an `std::function`, can be the result of `std::bind`
+         */
+        void on_created(EventCallback callback);
 
-        void on_modified(EventCallback& callback)
-        {
-            this->modfied_cb = callback;
-        }
+        /**
+         * @brief Register function to be called when a file is modified.
+         * @param callback an `std::function`, can be the result of `std::bind`
+         */
+        void on_modified(EventCallback callback);
 
-        void on_deleted(EventCallback& callback)
-        {
-            this->deleted_cb = callback;
-        }
+        /**
+         * @brief Register function to be called when a file is deleted.
+         * @param callback an `std::function`, can be the result of `std::bind`
+         */
+        void on_deleted(EventCallback callback);
+
+        /**
+         * @brief Check whether thread is still alive
+         */
+        bool good();
 };
 
 #endif /* FILEOBSERVER_H */
